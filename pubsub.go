b21b1a5aaf97ee5c61d287d56d5d43bee7576abc
Yasuhiro Tajiri <yasuhiro.tajiri@gmail.com>
2021-04-27 08:45:13 +0900

    fixed

--- pubsub.go
+++ pubsub.go
@@ -13,12 +13,29 @@ type Client struct{
 	client *pubsub.Client
 }
 
+type Topic struct{
+	topic *pubsub.Topic
+}
+
 func init() {
 	modules.Register("k6/x/CloudPubSub", new(PubSub))
 }
 
-func (* PubSub) XClient(ctxPtr *context.Context) interface{}{
+func (* PubSub) XClient(ctxPtr *context.Context, project string) interface{}{
 	rt := common.GetRuntime(*ctxPtr)
-	//cli, _ := pubsub.NewClient(*ctxPtr, project)
-	return common.Bind(rt, &Client{}, ctxPtr)
+	cli, _ := pubsub.NewClient(*ctxPtr, project)
+	return common.Bind(rt, &Client{client: cli}, ctxPtr)
+}
+
+func (cli *Client) XTopic(ctxPtr *context.Context, topicName string) interface{} {
+	rt := common.GetRuntime(*ctxPtr)
+	topic := cli.client.Topic(topicName)
+	return common.Bind(rt, &Topic{topic: topic}, ctxPtr)
+}
+
+func (t *Topic)XPublish(ctxPtr *context.Context, msg string){
+	m := &pubsub.Message{
+		Data: []byte(msg),
+	}
+	t.topic.Publish(*ctxPtr, m)
 }
