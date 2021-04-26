d537d5c5d10ee51c36b7d63cfdf12885e90ec23a
Yasuhiro Tajiri <yasuhiro.tajiri@gmail.com>
2021-04-27 08:22:01 +0900

    fixed

--- pubsub.go
+++ pubsub.go
@@ -4,16 +4,20 @@ import (
 	"context"
 
 	"github.com/loadimpact/k6/js/modules"
+	"github.com/loadimpact/k6/js/common"
 	"cloud.google.com/go/pubsub"
 )
 
 type PubSub struct {}
-
+type client struct{
+	client *pubsub.Client
+}
 func init() {
 	modules.Register("k6/x/CloudPubSub", new(PubSub))
 }
 
-func (* PubSub) XClient(ctx context.Context, project string) *pubsub.Client {
-	cli, _ := pubsub.NewClient(ctx, project)
-	return cli
+func (* PubSub) XClient(ctxPtr *context.Context, project string) interface{}{
+	rt := common.GetRuntime(*ctxPtr)
+	cli, _ := pubsub.NewClient(*ctxPtr, project)
+	return common.Bind(rt, &client{client: cli}, ctxPtr)
 }
